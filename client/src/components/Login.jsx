a87170ed0307f1ea543439708ba4bf6d62d1a486
Iiro <iiro.valimaa@gmail.com>
2022-09-02 01:59:54 +0300

    Login formin fiksaamista

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,14 +3,14 @@ import '../styles/Login.css';
 
 function Login() {
     return (
-        <div className="">
+        <div className="login">
             <h1>Login</h1>
             <div className="login-form">
                     <label className="login-form-label">Sähköposti : </label>
                     <input className="login-form-input" type="text" />
                     <label className="login-form-label">Salasana : </label>
                     <input className="login-form-input" type="password" name="salasana" id="salasana" />
-                    <div className="nappi">
+                    <div className="nappi-container">
                         <button type="submit">Kirjaudu</button>
                     </div>
             </div>
